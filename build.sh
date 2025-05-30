f18185fe8ca3bd9da0d194ef54ff4341ab96b9f4
Bob Dröge <b.e.droge@rug.nl>
2025-05-30 17:10:33 +0200

    pull build container image once for the demo and cache it

--- build.sh
+++ build.sh
@@ -8,4 +8,9 @@ fi
 module purge
 export EESSI_REPOS_CFG_DIR_OVERRIDE=~/stack-on-top-of-eessi-demo/repos.cfg
 
-~/software-layer/eessi_container.sh --mode run -r webinar-demo,access=rw -r software.eessi.io -b ~/software-layer,$PWD -- ./install-on-top-of-EESSI.sh $*
+# For the demo, we cache the container image in the home directory to prevent it from being redownloaded for every run
+if [[ ! -f ~/build-node_debian11.sif ]]; then
+    apptainer pull docker://ghcr.io/eessi/build-node:debian11
+fi
+
+~/software-layer/eessi_container.sh --mode run -c -r webinar-demo,access=rw -r software.eessi.io -b ~/software-layer,$PWD -- ./install-on-top-of-EESSI.sh $*
